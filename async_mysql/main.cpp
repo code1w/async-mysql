88b34145ba61b534a3b775028815123006243401
zhang xiao bin <zhangxiaobin@ztgame.com>
2020-08-10 22:20:22 +0800

    增加select

--- async_mysql/main.cpp
+++ async_mysql/main.cpp
@@ -7,6 +7,15 @@
 #include <mysql/mysql.h>
 #include <event.h>
 #include <uv.h>
+#include <functional>
+#include <thread>
+#include <mutex>
+
+#ifdef _WIN32
+#include <Winsock2.h>
+#else
+#include <sys/select.h>
+#endif /* _WIN32 */
 
 typedef struct mysql_context_s {
 	uv_poll_t poll_handle;
@@ -16,14 +25,68 @@ typedef struct mysql_context_s {
 
 } mysql_context_t;
 
-std::vector<MYSQL*> conns;
+std::vector<mysql_context_t*> conns;
 std::unordered_map<unsigned long long , event*> events;
+std::vector<std::string> query_sql;
+std::mutex mutex;
+
 void query(const std::string& sql,MYSQL *m);
 void hand_libevent_event(intptr_t fd, short what, void* _userdata);
 void hand_libuv_event(uv_poll_t* handle, int status, int what);
 void mysql_write(MYSQL* m);
 
 void mysql_read_result(MYSQL*);
+
+int hand_times = 0;
+
+
+// ========================================================
+// select 
+
+#ifdef _WIN32
+typedef SOCKET fd_t;
+#define __TACOPIE_INVALID_FD INVALID_SOCKET
+#else
+typedef int fd_t;
+#define __TACOPIE_INVALID_FD -1
+#endif /* _WIN32 */
+
+fd_set m_rd_set;
+fd_set m_wr_set;
+std::unordered_map<fd_t, mysql_context_t*> m_tracked_sockets;
+std::vector<fd_t> m_polled_fds;
+int init_poll_fds_info(void);
+void process_events(void);
+void select_rd_event_cb(fd_t, mysql_context_t);
+void select_wr_event_cb(fd_t, mysql_context_t);
+
+void query_in_loop(const std::string& sql)
+{
+	std::lock_guard<std::mutex> l(mutex);
+	query_sql.push_back(sql);
+}
+
+int init_poll_fds_info(void) {
+
+	m_polled_fds.clear();
+	FD_ZERO(&m_rd_set);
+	FD_ZERO(&m_wr_set);
+
+	int ndfs = 0;
+	for (const auto& socket : m_tracked_sockets) {
+		const auto& fd          = socket.first;
+		const auto& socket_info = socket.second;
+
+		FD_SET(fd, &m_rd_set);
+		//FD_SET(fd, &m_wr_set);
+		m_polled_fds.push_back(fd);
+		if(fd > ndfs)
+		{
+			ndfs = (int)fd;
+		}
+	}
+	return ndfs + 1;
+}
 
 void hand_libevent_event(intptr_t fd, short what, void* _userdata)
 {
@@ -56,6 +119,8 @@ void hand_libevent_event(intptr_t fd, short what, void* _userdata)
 
 void hand_libuv_event(uv_poll_t* handle, int status, int what)
 {
+	hand_times++;
+	
 	mysql_context_t* context = (mysql_context_t*)handle->data;
 	
 	/*
@@ -65,10 +130,11 @@ void hand_libuv_event(uv_poll_t* handle, int status, int what)
 		(what&UV_WRITABLE)   ? " write" : "");
 	*/
 
-	printf("mysql_context_t index %d\n", context->index);
+	
 
 	if (what & UV_READABLE)
 	{
+		printf("hand_libuv_event times %d\n", hand_times);
 		mysql_read_result(context->m);
 	}
 	if (what & UV_WRITABLE)
@@ -80,10 +146,12 @@ void hand_libuv_event(uv_poll_t* handle, int status, int what)
 
 void mysql_write(MYSQL* m)
 {
-	//query("select * from bag;", m);
+	query("select * from bag;", m);
 }
 
 void mysql_read_result(MYSQL* m) {
+	//mysql_read_query_result(m);
+	
 	if (0 == mysql_read_query_result(m)) {
 		MYSQL_RES *res = mysql_store_result(m);
 		MYSQL_ROW row;
@@ -100,28 +168,29 @@ void mysql_read_result(MYSQL* m) {
 	} else {
 		std::cout <<" mysql_read_query_result() failed for fd = "<< m->net.fd << " "<<mysql_error(m)<< ":"<<mysql_errno(m)<< std::endl;
 	}
-	
 }
 
 void connect_mysql_server()
 {
-	for (int i = 0; i < 1; i++)
+	for (int i = 0; i < 4; i++)
 	{
-		MYSQL* m = nullptr;
-		m = mysql_init(nullptr);
-		if (!m)
+		mysql_context_t* c = new mysql_context_t;
+		
+		c->m = mysql_init(nullptr);
+		if (!c->m)
 		{
 			std::cout<<"mysql init error"<< std::endl;
 			return;
 		}
-		m->free_me = 1;
-		auto ret = mysql_real_connect(m,"192.168.150.79", "root","123456","gamesh_zxb",3306,NULL,0);
+		c->m->free_me = 1;
+		auto ret = mysql_real_connect(c->m,"192.168.150.79", "root","123456","gamesh_zxb",3306,NULL,0);
 		if (!ret)
 		{
-			std::cout<<"Mysql conn err : "<<mysql_error(m) << ", errno :"<< mysql_errno(m)<< std::endl;
+			std::cout<<"Mysql conn err : "<<mysql_error(c->m) << ", errno :"<< mysql_errno(c->m)<< std::endl;
 			return;
 		}
-		conns.push_back(m);
+		conns.push_back(c);
+		m_tracked_sockets[c->m->net.fd] = c;
 	}
 }
 
@@ -140,15 +209,17 @@ void connect_mysql_server(mysql_context_t* context)
 		std::cout<<"Mysql conn err : "<<mysql_error(context->m) << ", errno :"<< mysql_errno(context->m)<< std::endl;
 		return;
 	}
+	m_tracked_sockets[context->m->net.fd] = context;
+	conns.push_back(context);
 }
 
 void bind_event(struct event_base* evbase)
 {
-	for (auto m : conns)
+	for (auto c : conns)
 	{
-		auto e = event_new(evbase, m->net.fd, EV_READ |EV_WRITE | EV_PERSIST, hand_libevent_event, m);
+		auto e = event_new(evbase, c->m->net.fd, EV_READ | EV_PERSIST, hand_libevent_event, c->m);
 		event_add(e, NULL);
-		events[m->net.fd] = e;
+		events[c->m->net.fd] = e;
 	}
 
 }
@@ -170,13 +241,14 @@ void query(const std::string& sql, mysql_context_t* c)
 
 void test_libevent()
 {
+	printf("======== test_libevent ========\n");
 	struct event_base* evbase = event_base_new();
 	connect_mysql_server();
+	query("select * from account;", conns[0]);
+	query("select * from bag;", conns[1]);
+	query("select * from account;", conns[2]);
+	query("select * from bag;", conns[3]);
 	bind_event(evbase);
-	query("select * from account;", conns[0]);
-	query("select * from bag;", conns[0]);
-	query("select * from account;", conns[0]);
-	query("select * from bag;", conns[0]);
 	event_base_dispatch(evbase);
 }
 
@@ -185,24 +257,32 @@ void bind_libuv(uv_loop_t* loop, mysql_context_t* context)
 {
 	uv_poll_init_socket(loop, &context->poll_handle, context->m->net.fd);
 	context->poll_handle.data = context;
-	uv_poll_start(&context->poll_handle , UV_READABLE  | UV_WRITABLE | UV_DISCONNECT, hand_libuv_event);
+	uv_poll_start(&context->poll_handle , UV_READABLE   | UV_DISCONNECT, hand_libuv_event);
 }
 
 void test_libuv()
 {
+	printf("======== test_libuv ========\n");
 	uv_loop_t* loop = uv_default_loop();
 
 	mysql_context_t* c1 = new mysql_context_t;
+	mysql_context_t* c2 = new mysql_context_t;
+	mysql_context_t* c3 = new mysql_context_t;
+	mysql_context_t* c4 = new mysql_context_t;
+
 	connect_mysql_server(c1);
 	bind_libuv(loop, c1);
-	mysql_context_t* c2 = new mysql_context_t;
 	connect_mysql_server(c2);
 	bind_libuv(loop, c2);
+	connect_mysql_server(c3);
+	bind_libuv(loop, c3);
+	connect_mysql_server(c4);
+	bind_libuv(loop, c4);
 
 	query("select * from account;",c1->m);
-	query("select * from bag;",c1->m);
-	query("select * from account;",c1->m);
-	query("select * from bag;",c1->m);
+	query("select * from bag;",c2->m);
+	query("select * from account;",c3->m);
+	query("select * from bag;",c4->m);
 
 #if 0
 
@@ -212,6 +292,75 @@ void test_libuv()
 	query("select * from bag;",c2);
 #endif 
 	int r = uv_run(loop, UV_RUN_DEFAULT);
+}
+
+
+
+void select_rd_event_cb(fd_t fd, mysql_context_t* c)
+{
+	mysql_read_result(c->m);
+}
+
+void select_wr_event_cb(fd_t fd, mysql_context_t* c)
+{
+
+}
+
+void process_events(void) {
+
+	for (const auto& fd : m_polled_fds) {
+
+		auto it = m_tracked_sockets.find(fd);
+
+		if (it == m_tracked_sockets.end()) { continue; }
+
+		auto& socket = it->second;
+
+		if (FD_ISSET(fd, &m_rd_set)) {
+			select_rd_event_cb(fd, socket);
+		}
+		if (FD_ISSET(fd, &m_wr_set)) {
+			select_wr_event_cb(fd, socket);
+		}
+	}
+}
+
+void process_query()
+{
+	std::vector<std::string> querys;
+	querys.swap(query_sql);
+	for (auto& sql : querys)
+	{
+		query(sql, conns[0]);
+	}
+}
+
+
+void poll()
+{
+	while (true) {
+		int ndfs = init_poll_fds_info();
+		struct timeval tv;
+		tv.tv_sec = 0;
+		tv.tv_usec = 10000;
+
+		if (select(ndfs, &m_rd_set, &m_wr_set, NULL, &tv) > 0) {
+			process_events();
+		}
+		else {
+			process_query();
+		}
+	}
+}
+
+void test_select()
+{
+	mysql_context_t* c1 = new mysql_context_t;
+	connect_mysql_server(c1);
+	std::thread worker = std::thread(poll);
+	query_in_loop(std::string("select * from account;"));
+	worker.join();
+
 }
 
 int main(int argc, char** argv)
@@ -226,7 +375,8 @@ int main(int argc, char** argv)
 	}
 #endif
 
-	test_libevent();
+	//test_libevent();
 	//test_libuv();
+	test_select();
 	return 0;
 }
